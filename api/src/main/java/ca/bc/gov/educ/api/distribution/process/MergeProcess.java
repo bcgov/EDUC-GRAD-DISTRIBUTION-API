5f095f9b014fe01bf4c85b380230b4a6eb90a9e5
s.karekkattumanasree <s.karekkattumanasreekumaran@cgi.com>
2022-06-29 14:08:06 -0700

    Local Download fix

--- api/src/main/java/ca/bc/gov/educ/api/distribution/process/MergeProcess.java
+++ api/src/main/java/ca/bc/gov/educ/api/distribution/process/MergeProcess.java
@@ -117,8 +117,10 @@ public class MergeProcess implements DistributionProcess {
 			}
 		}
 		createZipFile(batchId);
-		createControlFile(batchId,numberOfPdfs);
-		sftpUtils.sftpUploadBCMail(batchId);
+		if(processorData.getLocalDownload() == null) {
+			createControlFile(batchId, numberOfPdfs);
+			sftpUtils.sftpUploadBCMail(batchId);
+		}
 		long endTime = System.currentTimeMillis();
 		long diff = (endTime - startTime)/1000;
 		logger.info("************* TIME Taken  ************ {} secs",diff);
--- api/src/main/java/ca/bc/gov/educ/api/distribution/service/GradDistributionService.java
+++ api/src/main/java/ca/bc/gov/educ/api/distribution/service/GradDistributionService.java
@@ -36,8 +36,8 @@ public class GradDistributionService {
     @Autowired
     AccessTokenService accessTokenService;
 
-    public DistributionResponse distributeCredentials(String runType, Long batchId, Map<String, DistributionPrintRequest> mapDist, String activityCode, String accessToken) {
-        ProcessorData data = ProcessorData.builder().batchId(batchId).accessToken(accessToken).distributionResponse(null).mapDistribution(mapDist).activityCode(activityCode).build();
+    public DistributionResponse distributeCredentials(String runType, Long batchId, Map<String, DistributionPrintRequest> mapDist, String activityCode,String localDownload, String accessToken) {
+        ProcessorData data = ProcessorData.builder().batchId(batchId).accessToken(accessToken).distributionResponse(null).mapDistribution(mapDist).activityCode(activityCode).localDownload(localDownload).build();
         DistributionResponse disRes = new DistributionResponse();
         disRes.setMergeProcessResponse(processDistribution(runType,data).getMergeProcessResponse());
         return disRes;
