acdeb23b5820d23821d2c7da04bad5830b508f64
alexryba <alex.rybakov@gov.bc.ca>
2023-04-04 13:55:37 -0600

    GRAD2-1850 Year-End Disribution run (DISTRUN_YE) - reports School-Level (DISTREP_YE_SC) and District-Level (DISTREP_YE_SD)

--- api/src/main/java/ca/bc/gov/educ/api/distribution/process/BaseProcess.java
+++ api/src/main/java/ca/bc/gov/educ/api/distribution/process/BaseProcess.java
@@ -159,15 +159,6 @@ public abstract class BaseProcess implements DistributionProcess{
         return reportCount;
     }
 
-    protected Integer createDistrictSchoolNonGradReport(String accessToken, String schooLabelReportType, String districtReportType, String schoolReportType) {
-        Integer reportCount = 0;
-        final UUID correlationID = UUID.randomUUID();
-        reportCount += webClient.get().uri(String.format(educDistributionApiConstants.getSchoolDistrictStudentNonGradReport(),schooLabelReportType,districtReportType,schoolReportType))
-                .headers(h -> { h.setBearerAuth(accessToken); h.set(EducDistributionApiConstants.CORRELATION_ID, correlationID.toString()); })
-                .retrieve().bodyToMono(Integer.class).block();
-        return reportCount;
-    }
-
     protected int processDistrictSchoolDistribution(ProcessorData processorData, String schooLabelReportType, String districtReportType, String schoolReportType) {
         int numberOfPdfs = 0;
         if(StringUtils.isNotBlank(schooLabelReportType)) {
