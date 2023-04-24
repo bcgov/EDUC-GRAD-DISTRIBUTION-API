32072dbbb183ddc4fd55ef43ded48881a6728bce
alexryba <alex.rybakov@gov.bc.ca>
2023-04-24 11:00:36 -0600

    timezone config

--- api/src/main/java/ca/bc/gov/educ/api/distribution/config/EducDistributionApiConfig.java
+++ api/src/main/java/ca/bc/gov/educ/api/distribution/config/EducDistributionApiConfig.java
@@ -1,15 +1,18 @@
 package ca.bc.gov.educ.api.distribution.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
 import java.io.FileFilter;
+import java.util.TimeZone;
 
 @Configuration
 public class EducDistributionApiConfig {
@@ -44,5 +47,18 @@ public class EducDistributionApiConfig {
                     !name.contains("undertow") &&
                     !name.contains("hsperfdata");
         };
+    }
+
+    @Bean
+    ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder.createXmlMapper(false)
+                // Set timezone for JSON serialization as system timezone
+                .timeZone(TimeZone.getDefault())
+                .build();
+    }
+
+    @Bean
+    Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder();
     }
 }
