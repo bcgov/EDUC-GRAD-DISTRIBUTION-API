626bd0cfa38f168957cd16d1f1f8096a08cec8df
arybakov <alex.rybakov@cgi.com>
2023-06-13 13:49:04 -0600

    Test Coverage

--- api/src/test/java/ca/bc/gov/educ/api/distribution/service/PostingProcessServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/distribution/service/PostingProcessServiceTest.java
@@ -1,19 +1,22 @@
 package ca.bc.gov.educ.api.distribution.service;
 
 import ca.bc.gov.educ.api.distribution.model.dto.DistributionResponse;
+import ca.bc.gov.educ.api.distribution.model.dto.SchoolReports;
 import ca.bc.gov.educ.api.distribution.util.EducDistributionApiConstants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.wildfly.common.Assert;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 import static ca.bc.gov.educ.api.distribution.util.EducDistributionApiUtils.*;
@@ -24,11 +27,13 @@ import static org.mockito.Mockito.when;
 @RunWith(SpringRunner.class)
 public class PostingProcessServiceTest {
 
-    @Mock
+    @Autowired
     private PostingDistributionService postingDistributionService;
     @Autowired
     private EducDistributionApiConstants educDistributionApiConstants;
 
+    @Mock
+    RestService restService;
     @Mock
     private WebClient.RequestHeadersSpec requestHeadersMock;
     @Mock
@@ -50,15 +55,38 @@ public class PostingProcessServiceTest {
         response.setActivityCode(YEARENDDIST);
 
         when(this.webClient.get()).thenReturn(this.requestHeadersUriMock);
-        when(this.requestHeadersUriMock.uri(String.format(educDistributionApiConstants.getSchoolDistrictYearEndReport(), "", DISTREP_YE_SD, DISTREP_YE_SC))).thenReturn(this.requestHeadersMock);
+        when(this.requestHeadersUriMock.uri(String.format(educDistributionApiConstants.getSchoolDistrictYearEndReport(), null, DISTREP_YE_SD, DISTREP_YE_SC))).thenReturn(this.requestHeadersMock);
         when(this.requestHeadersMock.headers(any(Consumer.class))).thenReturn(this.requestHeadersMock);
         when(this.requestHeadersMock.retrieve()).thenReturn(this.responseMock);
         when(this.responseMock.onStatus(any(), any())).thenReturn(this.responseMock);
         when(this.responseMock.bodyToMono(Integer.class)).thenReturn(Mono.just(1));
 
-        Mockito.doReturn(1).when(postingDistributionService).createDistrictSchoolYearEndReport("", DISTREP_YE_SD, DISTREP_YE_SC);
+        when(this.webClient.get()).thenReturn(this.requestHeadersUriMock);
+        when(this.requestHeadersUriMock.uri(String.format(educDistributionApiConstants.getSchoolReportsByReportType(), ADDRESS_LABEL_SCHL, ""))).thenReturn(this.requestHeadersMock);
+        when(this.requestHeadersMock.headers(any(Consumer.class))).thenReturn(this.requestHeadersMock);
+        when(this.requestHeadersMock.retrieve()).thenReturn(this.responseMock);
+        when(this.responseMock.onStatus(any(), any())).thenReturn(this.responseMock);
+        when(this.responseMock.bodyToMono(new ParameterizedTypeReference<List<SchoolReports>>() {
+        })).thenReturn(Mono.just(new ArrayList<>()));
+
+        when(this.webClient.get()).thenReturn(this.requestHeadersUriMock);
+        when(this.requestHeadersUriMock.uri(String.format(educDistributionApiConstants.getSchoolReportsByReportType(), DISTREP_YE_SD, ""))).thenReturn(this.requestHeadersMock);
+        when(this.requestHeadersMock.headers(any(Consumer.class))).thenReturn(this.requestHeadersMock);
+        when(this.requestHeadersMock.retrieve()).thenReturn(this.responseMock);
+        when(this.responseMock.onStatus(any(), any())).thenReturn(this.responseMock);
+        when(this.responseMock.bodyToMono(new ParameterizedTypeReference<List<SchoolReports>>() {
+        })).thenReturn(Mono.just(new ArrayList<>()));
+
+        when(this.webClient.get()).thenReturn(this.requestHeadersUriMock);
+        when(this.requestHeadersUriMock.uri(String.format(educDistributionApiConstants.getSchoolReportsByReportType(), DISTREP_YE_SC, ""))).thenReturn(this.requestHeadersMock);
+        when(this.requestHeadersMock.headers(any(Consumer.class))).thenReturn(this.requestHeadersMock);
+        when(this.requestHeadersMock.retrieve()).thenReturn(this.responseMock);
+        when(this.responseMock.onStatus(any(), any())).thenReturn(this.responseMock);
+        when(this.responseMock.bodyToMono(new ParameterizedTypeReference<List<SchoolReports>>() {
+        })).thenReturn(Mono.just(new ArrayList<>()));
+
         var result = this.postingDistributionService.postingProcess(response);
-        Assert.assertNotNull(result);
+        Assert.assertTrue(result);
     }
 
 }
