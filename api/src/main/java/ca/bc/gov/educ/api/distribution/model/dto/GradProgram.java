50b5798c72e50f26104a397eb19b9df2fbe8334c
Jinil Sung <infstar@gmail.com>
2024-02-29 15:33:59 -0800

    GRAD2-2525: DTO update to sync.
    GRAD2-2525: DTO update to sync.

--- api/src/main/java/ca/bc/gov/educ/api/distribution/model/dto/GradProgram.java
+++ api/src/main/java/ca/bc/gov/educ/api/distribution/model/dto/GradProgram.java
@@ -7,11 +7,21 @@ public class GradProgram implements Serializable {
 
     private Code code;
 
+    private String expiryDate;
+
     public Code getCode() {
         return code;
     }
 
     public void setCode(Code value) {
         this.code = value;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String value) {
+        this.expiryDate = value;
     }
 }
